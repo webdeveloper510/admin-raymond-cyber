94188888b23c98bd4d5ab5a87c8577371209c5f8
webdeveloper510 <developerweb6@gmail.com>
2025-10-25 16:55:55 +0530

    changes

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
       console.log("Login Response:", response);
       if (response.code === "200") {
 localStorage.setItem("access_token", response.data.access_token);
-        navigate("/dashboard");
+        navigate("/admin/dashboard");
       } else {
         toast.error(response.message || "Invalid credentials");
       }
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,36 +1,27 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../Auth/Login.jsx";
-// import Signup from "../Auth/Signup.jsx";
-// import ForgotPassword from "../Auth/ForgotPassword.jsx";
-// // import ResetPassword from "../Auth/ResetPassword.jsx";
-// import TwoFactorAuth from "../Auth/OtpSend.jsx";
-// import TwoFactorVerify from "../Auth/OtpVerify.jsx";
 import DashboardLayout from "../Sidebar/sidebar.jsx";
-// import TransactionSuccess from "../Auth/TranscationSuccess.jsx";
-// import NotFound from "../pages/NotFound.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import PublicRoute from "./PublicRoute.jsx";
-// import PaymentSuccessRoute from "./PaymentSuccessCheck.jsx";
+
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* Redirect root "/" to /admin/login */}
+      <Route path="/" element={<Navigate to="/admin/login" replace />} />
+
       {/* Public routes */}
       <Route element={<PublicRoute />}>
-        <Route path="/" element={<Login />} />
-          {/* <Route path="/2fa" element={<TwoFactorAuth />} />
-           <Route path="/otp-verify" element={<TwoFactorVerify />} /> */}
-        {/* <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword/>}/> */}
+        <Route path="/admin/login" element={<Login />} />
       </Route>
 
       {/* Protected routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/dashboard" element={<DashboardLayout />} />
+        <Route path="/admin/dashboard" element={<DashboardLayout />} />
       </Route>
 
-      {/* 404 Page */}
+      {/* Optional 404 */}
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
